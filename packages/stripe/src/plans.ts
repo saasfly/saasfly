6348423d71e7b0274aea2dec870c9426dbd406e2
tianzx <tianzx@aliyun.com>
2024-04-01 11:31:24 +0800

    🔒 feat(stripe): Add plans.ts file and update .eslintignore

--- packages/stripe/src/plans.ts
+++ packages/stripe/src/plans.ts
@@ -2,13 +2,18 @@ import { SubscriptionPlan } from "@saasfly/db";
 
 import { env } from "./env.mjs";
 
+
 export const PLANS: Record<
   string,
   (typeof SubscriptionPlan)[keyof typeof SubscriptionPlan]
 > = {
+  // @ts-ignore
   [env.NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID]: SubscriptionPlan.PRO,
-  [env.NEXT_PUBLIC_STRIPE_PRO_YEARLY_PRICE_ID]: SubscriptionPlan.PRO,
+  // @ts-ignore
+  [env.NEXT_PUBLIC_STRIPE_PRO_YEARLY_PRICE_ID ]: SubscriptionPlan.PRO,
+  // @ts-ignore
   [env.NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PRICE_ID]: SubscriptionPlan.BUSINESS,
+  // @ts-ignore
   [env.NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PRICE_ID]: SubscriptionPlan.BUSINESS,
 };
 
