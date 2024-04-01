cbbe5c79946f2a30f469bbbea9399d8d442fa3ba
tianzx <tianzx@aliyun.com>
2024-04-01 11:35:21 +0800

    🐛 fix(stripe/plans): remove empty line and format Stripe plan IDs

--- packages/stripe/src/plans.ts
+++ packages/stripe/src/plans.ts
@@ -2,7 +2,6 @@ import { SubscriptionPlan } from "@saasfly/db";
 
 import { env } from "./env.mjs";
 
-
 export const PLANS: Record<
   string,
   (typeof SubscriptionPlan)[keyof typeof SubscriptionPlan]
@@ -10,7 +9,7 @@ export const PLANS: Record<
   // @ts-ignore
   [env.NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID]: SubscriptionPlan.PRO,
   // @ts-ignore
-  [env.NEXT_PUBLIC_STRIPE_PRO_YEARLY_PRICE_ID ]: SubscriptionPlan.PRO,
+  [env.NEXT_PUBLIC_STRIPE_PRO_YEARLY_PRICE_ID]: SubscriptionPlan.PRO,
   // @ts-ignore
   [env.NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PRICE_ID]: SubscriptionPlan.BUSINESS,
   // @ts-ignore
