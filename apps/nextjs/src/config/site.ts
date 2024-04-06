73f2434f9e3a446dee00ed151cef2fc3a69ca3e8
tianzx <tianzx@aliyun.com>
2024-04-06 16:48:52 +0800

    🐛 fix(site): update site config and other minor changes

--- apps/nextjs/src/config/site.ts
+++ apps/nextjs/src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "saasfly",
+  name: "Saasfly",
   description: "We provide an easier way to build saas service in production",
   url: "https://github.com/saasfly/saasfly",
   ogImage: "",
--- packages/common/src/config/site.ts
+++ packages/common/src/config/site.ts
@@ -1,9 +1,9 @@
 export const siteConfig = {
-  name: "nok8s",
+  name: "Saasfly",
   description: "We provide an easier way to build saas service in production",
-  url: "https://github.com/nok8s-pro",
-  ogImage: "blob:https://vercel.com/eba52a69-e2ff-4707-876b-da29a08676ea",
+  url: "https://github.com/saaslfy/saasfly",
+  ogImage: "",
   links: {
-    github: "https://github.com/nok8s-pro",
+    github: "https://github.com/saaslfy",
   },
 };
