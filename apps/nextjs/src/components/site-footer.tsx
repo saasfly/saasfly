b73b17dec0eeb97851900969d8e7bcb093a9d0aa
nextify <tianzx@aliyun.com>
2024-04-07 22:19:43 +0800

    Remove base create button (#25)
    * 🔥 refactor(base-create-button): remove unused component
    * 🐛 Fix: Update site-footer copyright year calculation

--- apps/nextjs/src/components/site-footer.tsx
+++ apps/nextjs/src/components/site-footer.tsx
@@ -9,8 +9,7 @@ function getCopyrightText(
   dict: Record<string, string | Record<string, string>>,
 ) {
   const currentYear = new Date().getFullYear();
-  const copyrightTemplate = String(dict.copyright); // 从字典中获取版权信息模板
-  // 替换占位符 ${currentYear} 为实际的年份
+  const copyrightTemplate = String(dict.copyright);
   return copyrightTemplate?.replace("${currentYear}", String(currentYear));
 }
 
