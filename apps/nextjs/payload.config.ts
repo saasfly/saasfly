1ba855d9cfdd9b5e05087af51a4a0dc209e58ee2
tianzx <tianzx@aliyun.com>
2024-04-14 14:03:11 +0800

    📽️ feat(nextjs): add Payload CMS integration

---
+++ apps/nextjs/payload.config.ts
@@ -0,0 +1,81 @@
+import path from 'path'
+// import { postgresAdapter } from '@payloadcms/db-postgres'
+import { en } from 'payload/i18n/en'
+import {
+
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+//import { slateEditor } from '@payloadcms/richtext-slate'
+// import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { buildConfig } from 'payload/config'
+import sharp from 'sharp'
+import { fileURLToPath } from 'url'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  //editor: slateEditor({}),
+  editor: lexicalEditor(),
+  collections: [
+    {
+      slug: 'users',
+      auth: true,
+      access: {
+        delete: () => false,
+        update: () => false,
+      },
+      fields: [],
+    }
+  ],
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URL || '',
+    },
+  }),
+
+  /**
+   * Payload can now accept specific translations from 'payload/i18n/en'
+   * This is completely optional and will default to English if not provided
+   */
+  i18n: {
+    supportedLanguages: { en },
+  },
+
+  admin: {
+    autoLogin: {
+      email: 'dev@payloadcms.com',
+      password: 'test',
+      prefillOnly: true,
+    },
+  },
+
+  async onInit(payload) {
+    const existingUsers = await payload.find({
+      collection: 'users',
+      limit: 1,
+    })
+
+    if (existingUsers.docs.length === 0) {
+      await payload.create({
+        collection: 'users',
+        data: {
+          email: 'dev@payloadcms.com',
+          password: 'test',
+        },
+      })
+    }
+  },
+  // Sharp is now an optional dependency -
+  // if you want to resize images, crop, set focal point, etc.
+  // make sure to install it and pass it to the config.
+
+  // This is temporary - we may make an adapter pattern
+  // for this before reaching 3.0 stable
+  sharp,
+})
--- apps/nextjs/src/app/[lang]/layout.tsx
+++ apps/nextjs/src/app/[lang]/layout.tsx
@@ -24,7 +24,7 @@ const fontSans = FontSans({
 
 // Font files can be colocated inside of `pages`
 const fontHeading = localFont({
-  src: "../styles/fonts/CalSans-SemiBold.woff2",
+  src: "../../styles/fonts/CalSans-SemiBold.woff2",
   variable: "--font-heading",
 });
 
