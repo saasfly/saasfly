9c0e4b8048efc2f9195df022fa2072e289f4f909
子文 <354928594@qq.com>
2024-05-10 18:02:53 +0800

    fix: prevent form submit multiple times (#53)
    * fix: 🐛 prevent form submit multiple times

--- apps/nextjs/src/components/k8s/cluster-config.tsx
+++ apps/nextjs/src/components/k8s/cluster-config.tsx
@@ -24,6 +24,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@saasfly/ui/form";
+import * as Icons from "@saasfly/ui/icons";
 import { Input } from "@saasfly/ui/input";
 import { Label } from "@saasfly/ui/label";
 import {
@@ -341,7 +342,12 @@ export function ClusterConfig({ cluster, params: { lang } }: ClusterProps) {
             </div>
           </CardContent>
           <div className="w-2/3 space-y-6 p-6 pt-0">
-            <Button type="submit">Submit</Button>
+            <Button type="submit" disabled={_isSaving}>
+            {_isSaving && (
+              <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+              Submit
+              </Button>
           </div>
         </Card>
       </form>
