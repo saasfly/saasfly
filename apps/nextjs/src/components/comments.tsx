59c74e36ea7434d1af67bc96540cabbe4746f22d
yaobingxun <yaobingxun@meituan.com>
2024-06-04 23:04:09 +0800

    update ui

--- apps/nextjs/src/components/comments.tsx
+++ apps/nextjs/src/components/comments.tsx
@@ -80,7 +80,7 @@ const ReviewCard = ({
 
 const Comments = () => {
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background py-20 md:shadow-xl">
+    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background py-20">
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
           <ReviewCard key={review.username} {...review} />
