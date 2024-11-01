41b2e2bf1aa372d08617bff8d0aed8d5f4cfbd3b
洪健翔 Hung, Chien-Hsiang <iworkhard@hsiang.eu.org>
2024-11-01 04:27:43 +0000

    run bun run format:fix

--- packages/ui/src/icons.tsx
+++ packages/ui/src/icons.tsx
@@ -32,7 +32,7 @@ export const Laptop = Lucide.Laptop;
 export const Languages = Lucide.Languages;
 export const Cluster = Lucide.CloudCog;
 export const Search = Lucide.Search;
-export const Menu = Lucide.Menu
+export const Menu = Lucide.Menu;
 
 export const System: Icon = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
